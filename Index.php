00a327a81034daf944ccde4d602d8f0434fb29da
dada971 <darius.brosseau@gmail.com>
2019-04-26 21:14:15 +0200

    modifié :         Index.php 	modifié :         styles.css

--- Index.php
+++ Index.php
@@ -8,8 +8,8 @@
 </head>
 <body>
 <?php
-    $nameErr=$prenomErr=$emailErr=$websitErr=$commentaireErr=$genreErr="";
-     $name=$prenom=$email=$website=$commentaire=$genre="";
+    $nameErr=$commandeErr=$commentaireErr=$livraisonErr=$uniteErr="";
+     $name=$commande=$commentaire=$livraison=$unite="";
     
    if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if(empty($_POST["name"]))
@@ -19,44 +19,33 @@
         else{
             $name=test_input($_POST["name"]);
         }
-        
-        if(empty($_POST["prenom"]))
+        if(empty($_POST["commande"]))
         {
-            $prenomErr="erreur veuiller rentrer un prenom";
+            $commandeErr="";
         }
         else{
-            $prenom=test_input($_POST["prenom"]);
+            $commande=test_input($_POST["commande"]);
         }
-        
-        if(empty($_POST["email"]))
-        {
-            $emailErr="erreur veuiller rentrer un email";
-        }
-        else{
-            $email=test_input($_POST["email"]);
-        }
-        
-        if(empty($_POST["website"]))
-        {
-            $websiteErr="";
-        }
-        else{
-            $website=test_input($_POST["website"]);
-        }
-        
         if(empty($_POST["commentaire"]))
         {
             $commentaireErr="";
         }
         else{
-            $name=test_input($_POST["commentaire"]);
+            $commentaire=test_input($_POST["commentaire"]);
         }
-        if(empty($_POST["genre"]))
+        if(empty($_POST["livraison"]))
         {
-            $genreErr="le genre est requis";
+            $livraisonErr="le type livraison est requis";
         }
         else{
-            $genre=test_input($_POST["genre"]);
+            $livraison=test_input($_POST["livraison"]);
+        }
+        if(empty($_POST["unite"]))
+        {
+            $uniteErr="";
+        }
+        else{
+            $unite=test_input($_POST["unite"]);
         }
     }
     
@@ -68,46 +57,49 @@
 }
     
     ?>
-<div>
-    <form class="forme" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<fieldset>
+    <legend>Article</legend>
+    <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         <label for="name">nom:</label>  
            <input type="text" name="name" id="name" placeholder="votre Nom" required maxlength="10"><span class="error">* <?php echo $nameErr;?></span>
         <br/><br/>
-        <label for="prenom">prenom:</label> 
-        <input type="text" name="prenom" id="prenom" placeholder="votre prenom" required maxlength="10"><span class="error">* <?php echo $prenomErr;?></span>
-        <br/><br/>
-        <label for="email">email:</label> 
-       <input type="email" name="email" id="email" placeholder="votre email" required maxlength="35"><span class="error">* <?php echo $emailErr;?></span>
-       <br/><br/>
-       <label for="website">website:</label> 
-        <input type="text" name="website" id="website">
-  <span class="error"><?php echo $websitErr;?></span>
-  <br><br>
-        commentaire: <textarea name="comment" rows="8" cols="40" placeholder="un pti commentaire"></textarea>
-        <br/><br/>
-        genre:     <input type="radio" name="genre" value="female">Female
-                    <input type="radio" name="genre" value="male">Male
-                    <input type="radio" name="genre" value="other">Other<span class="error">* <?php echo $nameErr;?></span>
+        
+       <label for="commande">commande:</label> 
+        <input type="number" min="1" max="100" name="commande" id="commande" required><span class="error"><?php echo $commandeErr;?></span>
+                    <select name="unite">
+                    <option value="kg" selected>kg</option>
+                    <option value="m3">m3</option>
+                    <option value="other">Other</option><span class="error">* <?php echo $uniteErr;?></span>
+                    </select>
                     <br><br>
+  <br><br>
+        
+        livraison:<select name="livraison">
+                    <option value="domicile">Domicile</option>
+                    <option value="magasin">Magasin</option>
+                    <option value="other">Other</option><span class="error">* <?php echo $livraisonErr;?></span>
+                    </select>
+                    <br><br>
+                    Spécifier une annotation: <textarea name="commentaire" rows="8" cols="40" placeholder="préciser une requète pour la livraison"></textarea>
+        <br/><br/>
         <input type="submit" value="soumettre" name="submit">
     </form>   
-    </div>
+    </fieldset>
       
   <?php
     
     
-echo "<h2>Your Input:</h2>";
+echo "<h2>Ma commande:</h2>";
+echo "livraison: ";
 echo $name;
 echo "<br>";
-echo $prenom;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
+echo "commande: ";
+echo $commande." ".$unite;
 echo "<br>";
 echo $commentaire;
 echo "<br>";
-echo $genre;
+echo "Type de livraison: ";
+echo $livraison;
 
     
     ?>
